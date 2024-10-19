94e2c1e212ba40eee8db2bbb5decbf72a6996cc0
evinyanitoz <eyanitoz@gmail.com>
2024-10-19 16:09:52 +0300

    Add project files.

---
+++ PIRAMIT/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace PIRAMIT
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("OLUŞTURULACAK PPİRAMİT YÜKSEKLİĞİ KAÇOLSUN? ");
+            int sayi=Convert.ToInt32(Console.ReadLine());
+            string yildiz=string.Empty;
+
+            for(int i =0;i<sayi;i++)
+            {
+                yildiz += "*";
+                Console.WriteLine(yildiz);
+            
+            
+            
+            }
+
+
+
+
+        }
+    }
+}
